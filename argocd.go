56a99af04fe30dfc4fa7526879dcc60c8293bf85
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2023-12-06 13:57:44 +0100

    feat: Bootstrap Mage Kubernetes library (#2)
    * 1:1 Copies what is in the engineering kubernetes image (https://github.com/coopnorge/engineering-docker-images/tree/main/images/devtools-kubernetes-v1beta1/context/magefiles)
    * Modify it to make it a library
    * Make it validate using golang devtools
    
    next up is adding features/tests

---
+++ argocd.go
@@ -0,0 +1,112 @@
+package magekubernetes
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/magefile/mage/sh"
+	"gopkg.in/yaml.v3"
+)
+
+// ArgoCDAppHelm contains the info for rendering a helm file
+type ArgoCDAppHelm struct {
+	ReleaseName string   `yaml:"releaseName"`
+	ValueFiles  []string `yaml:"valueFiles"`
+}
+
+// ArgoCDAppSource contains the info where to find the source for rendering
+type ArgoCDAppSource struct {
+	Helm ArgoCDAppHelm `yaml:"helm"`
+	Path string        `yaml:"path"`
+}
+
+// ArgoCDAppSpec contains the app source
+type ArgoCDAppSpec struct {
+	Source ArgoCDAppSource `yaml:"source"`
+}
+
+// ArgoCDAppMetadata contains the app name
+type ArgoCDAppMetadata struct {
+	Name string `yaml:"name"`
+}
+
+// ArgoCDApp contains the spec and metadata of an app
+type ArgoCDApp struct {
+	Spec     ArgoCDAppSpec     `yaml:"spec"`
+	Metadata ArgoCDAppMetadata `yaml:"metadata"`
+}
+
+func getArgoCDDeployments(repoURL string) ([]ArgoCDApp, error) {
+	var argoCDAppList []ArgoCDApp
+	env := map[string]string{}
+
+	if token, ok := os.LookupEnv("ARGOCD_API_TOKEN"); ok {
+		server, ok := env["ARGOCD_SERVER_NAME"]
+		if !ok {
+			return nil, fmt.Errorf("When using ARGOCD_API_TOKEN, you are also required to set ARGOCD_SERVER_NAME")
+		}
+		env["ARGOCD_API_TOKEN"] = token
+		env["ARGOCD_SERVER_NAME"] = server
+	} else {
+		err := sh.Run("argocd", "context")
+		if err != nil {
+			fmt.Println("Make use $HOME/.argocd is correctly mounted or use ARGOCD_API_TOKEN env var")
+			return nil, err
+		}
+	}
+
+	appYaml, err := sh.OutputWith(env, "argocd", "--grpc-web", "app", "list", "-r", repoURL, "-o", "yaml")
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal([]byte(appYaml), &argoCDAppList)
+	if err != nil {
+		return nil, err
+	}
+	return argoCDAppList, nil
+}
+
+func getArgoCDDiff(apps []ArgoCDApp) error {
+	env := map[string]string{"KUBECTL_EXTERNAL_DIFF": "dyff between --omit-header"}
+	if token, ok := os.LookupEnv("ARGOCD_API_TOKEN"); ok {
+		env["ARGOCD_API_TOKEN"] = token
+	}
+	for _, app := range apps {
+		diff, err := sh.OutputWith(env, "argocd", "--loglevel", "error", "--grpc-web", "app", "diff", app.Metadata.Name, "--refresh", "--local", app.Spec.Source.Path)
+		if sh.ExitStatus(err) == 2 {
+			return err
+		}
+		fmt.Println("---- Diff of " + app.Metadata.Name + "  ----")
+		fmt.Println(diff)
+	}
+	return nil
+}
+
+func listArgoCDDeployments() error {
+	repo, err := repoURL()
+	if err != nil {
+		return err
+	}
+	apps, err := getArgoCDDeployments(repo)
+	if err != nil {
+		return err
+	}
+	for _, trackedDeployment := range apps {
+		if trackedDeployment.Spec.Source.Helm.ReleaseName != "" {
+			fmt.Println("---")
+			fmt.Println("Found helm deployment with name: " + trackedDeployment.Metadata.Name)
+			fmt.Println("  path: " + trackedDeployment.Spec.Source.Path)
+			fmt.Println("  valueFiles: " + strings.Join(trackedDeployment.Spec.Source.Helm.ValueFiles, ", "))
+		} else if _, err := os.Stat(trackedDeployment.Spec.Source.Path + "/kustomize.yaml"); err == nil {
+			fmt.Println("---")
+			fmt.Println("Found kustomize deployment with name: " + trackedDeployment.Metadata.Name)
+			fmt.Println("  path: " + trackedDeployment.Spec.Source.Path)
+		} else {
+			fmt.Println("---")
+			fmt.Println("Found plain deployment with name: " + trackedDeployment.Metadata.Name)
+			fmt.Println("  path: " + trackedDeployment.Spec.Source.Path)
+		}
+	}
+	return nil
+}
---
+++ targets.go
@@ -0,0 +1,93 @@
+package magekubernetes
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/magefile/mage/mg" // mg contains helpful utility functions, like Deps
+	"github.com/magefile/mage/sh" // sh contains helpful utility functions, like RunV
+)
+
+// Validate Kubernetes manifests for ArgCD applications related to this repository
+func Validate() error {
+	templates, err := renderTemplates()
+	if err != nil {
+		return err
+	}
+	mg.Deps(mg.F(kubeScore, templates))
+	mg.Deps(mg.F(kubeConform, templates))
+	fmt.Println("Validation passed")
+	return nil
+}
+
+// KubeScore runs kube-score on Kubernetes manifests for ArgCD applications related to this repository
+func KubeScore() error {
+	templates, err := renderTemplates()
+	if err != nil {
+		return err
+	}
+	return kubeScore(templates)
+}
+
+// KubeConform runs kubeconform on Kubernetes manifests for ArgCD applications related to this repository
+func KubeConform() error {
+	templates, err := renderTemplates()
+	if err != nil {
+		return err
+	}
+	return kubeConform(templates)
+}
+
+// ArgoCDListApps show the apps related to this repository
+func ArgoCDListApps() error {
+	err := listArgoCDDeployments()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ArgoCDDiff runs a diff between local changes and the current running state in ArgoCD
+func ArgoCDDiff() error {
+	repo, err := repoURL()
+	if err != nil {
+		return err
+	}
+	apps, err := getArgoCDDeployments(repo)
+	if err != nil {
+		return err
+	}
+	err = getArgoCDDiff(apps)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func kubeScore(paths string) error {
+	cmdOptions := []string{
+		"score"}
+	out, err := sh.Output("kube-score", append(cmdOptions, strings.Split(paths, ",")...)...)
+	if err != nil {
+		fmt.Printf("kube-score returned exit code: %d\n Output:\n %v Error:\n %v\n", sh.ExitStatus(err), out, err)
+		return err
+	}
+	fmt.Println("kube-score passed")
+	return nil
+}
+
+func kubeConform(paths string) error {
+	cmdOptions := []string{
+		"-strict",
+		"-verbose",
+		"-ignore-missing-schemas",
+		"-schema-location", "default",
+		"-schema-location", "https://raw.githubusercontent.com/coopnorge/kubernetes-schemas/main/api-production/'{{ .ResourceKind }}{{ .KindSuffix }}.json'"}
+	out, err := sh.Output("kubeconform", append(cmdOptions, strings.Split(paths, ",")...)...)
+	if err != nil {
+		fmt.Printf("kubeconform returned exit code: %d\n Output:\n %v Error:\n %v\n", sh.ExitStatus(err), out, err)
+		return err
+	}
+	fmt.Println("kubeconform passed")
+	return nil
+}
