ed08cd7fbc22f346ba438ded51b69c88e020e925
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2023-12-20 15:31:14 +0100

    fix: get correct output of repoURL and test it (#10)
    * fix: get correct output of repoURL and test it
    
    * chore: added unable to parse remote url error type
    
    * chore: alternative error checking implementation
    
    * same validation corrections
    
    * revert argocd.go
    
    ---------
    
    Co-authored-by: Arun Poudel <arun.poudel@coop.no>

--- metadata.go
+++ metadata.go
@@ -1,9 +1,14 @@
 package magekubernetes
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/go-git/go-git/v5"
+)
+
+const (
+	unableToParseRemoteErr = "unable to parse remote url: %s"
 )
 
 func repoURL() (string, error) {
@@ -11,9 +16,20 @@ func repoURL() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	repoURL, err := repo.Remote("origin")
+	remote, err := repo.Remote("origin")
 	if err != nil {
 		return "", err
 	}
-	return strings.TrimSuffix(repoURL.String(), ".git"), nil
+	return gitRemoteParser(remote.String())
+}
+
+func gitRemoteParser(remote string) (string, error) {
+	url := strings.Fields(remote)[1]
+	if strings.HasPrefix(url, "https://") {
+		return strings.TrimSuffix(url, ".git"), nil
+	} else if strings.HasPrefix(url, "git@") {
+		toHTTPS := "https://github.com/" + strings.Split(url, ":")[1]
+		return strings.TrimSuffix(toHTTPS, ".git"), nil
+	}
+	return "", fmt.Errorf(unableToParseRemoteErr, url)
 }
---
+++ metadata_test.go
@@ -0,0 +1,38 @@
+package magekubernetes
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestRepoURL(t *testing.T) {
+	r, err := repoURL()
+	want := "https://github.com/coopnorge/mage-kubernetes-lib"
+	if r != want || err != nil {
+		t.Fatalf(`repoURL() failed. \nWant: %s\n got: %s\n error: %v`, want, r, err)
+	}
+}
+
+func TestGitRemoteParser(t *testing.T) {
+	var tests = []struct {
+		remote string
+		want   string
+		err    error
+	}{
+		{"origin     git@github.com:coopnorge/helloworld.git (fetch)", "https://github.com/coopnorge/helloworld", nil},
+		{"origin     https://github.com/coopnorge/helloworld.git (fetch)", "https://github.com/coopnorge/helloworld", nil},
+		{"origin     http://github.com/coopnorge/helloworld.git (fetch)", "", fmt.Errorf(unableToParseRemoteErr, "http://github.com/coopnorge/helloworld.git")},
+	}
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s,%s", tt.remote, tt.want)
+		t.Run(testname, func(t *testing.T) {
+			got, err := gitRemoteParser(tt.remote)
+			if got == tt.want &&
+				(errors.Is(err, tt.err) || err.Error() == tt.err.Error()) {
+				return
+			}
+			t.Errorf("\n got: %s,%v \nwant: %s,%v", got, err, tt.want, tt.err)
+		})
+	}
+}
